f431b85c91c4083ae98b77c65046eb4723074d20
AdamZvara <xzvara01@stud.fit.vutbr.cz>
2023-10-10 16:24:22 -0400

    a_star add shared pointers

--- sui-solution.cc
+++ sui-solution.cc
@@ -16,13 +16,17 @@ inline bool memUsageSucceeded(size_t limit) {
 
 class PathItem {
 public:
-	SearchState parent;
+	std::shared_ptr<SearchState> parent;
 	SearchAction action;
 
-	PathItem(const SearchState &p, const SearchAction& a) : parent(p), action(a) {}
+	PathItem(std::shared_ptr<SearchState> p, const SearchAction& a) : parent(move(p)), action(a) {}
 };
 
-std::vector<SearchAction> getPath(std::map<SearchState, PathItem> &paths, const SearchState &final, const SearchState &init) {
+std::vector<SearchAction> getPath(
+	std::map<std::shared_ptr<SearchState>, PathItem> &paths, 
+	std::shared_ptr<SearchState> final, 
+	std::shared_ptr<SearchState> init
+) {
 	std::vector<SearchAction> path;
 	auto current = paths.find(final);
 
@@ -52,41 +56,46 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
 
 class AStarFrontierItem {
 public:
-	SearchState state;
+	std::shared_ptr<SearchState> state;
 	int g_cost;
 	double f_cost;
 	
-	AStarFrontierItem(const SearchState &state, int g_cost, double f_cost):
-		state(state), g_cost(g_cost), f_cost(f_cost) {}
+	AStarFrontierItem(std::shared_ptr<SearchState> state, int g_cost, double f_cost):
+		state(move(state)), g_cost(g_cost), f_cost(f_cost) {}
 
-	// Constructor used for checking if state is in frontier
-	AStarFrontierItem(const SearchState &state): state(state) {}
+	// Constructor used for checking if state is in open
+	AStarFrontierItem(std::shared_ptr<SearchState> state): state(move(state)), g_cost(0), f_cost(0) {}
 
 	bool operator<(const AStarFrontierItem &other) const {
-		if (state == other.state) 
+		// std::cout << "AStarFrontierItem comparison" << std::endl;
+		if (*state.get() == *other.state.get()) 
 			return false;
 		return f_cost < other.f_cost;
 	}
 };
 
 std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {	
-	std::multiset<AStarFrontierItem> frontier;
-	std::set<SearchState> closed;
-	std::map<SearchState, PathItem> paths;
+	std::multiset<AStarFrontierItem> open;
+	std::set<std::shared_ptr<SearchState>> closed;
+	std::map<std::shared_ptr<SearchState>, PathItem> paths;
+
+	auto init_state_ptr = std::make_shared<SearchState>(init_state);
 
 	// Add initial state
-	frontier.insert(AStarFrontierItem(init_state, 0, compute_heuristic(init_state, *heuristic_)));
+	open.insert(AStarFrontierItem(init_state_ptr, 0, compute_heuristic(init_state, *heuristic_)));
 
-	while (!frontier.empty()) {
-		AStarFrontierItem current = *frontier.begin();
-		frontier.erase(frontier.begin());
-
+	while (!open.empty()) {
+		AStarFrontierItem current = *open.begin();
+		open.erase(open.begin());
 		closed.insert(current.state);
 
-		if (current.state.isFinal())
-			return getPath(paths, current.state, init_state);
+		auto current_state = current.state.get();
+		auto new_g_cost = current.g_cost + 1;
 
-		for (SearchAction &action : current.state.actions()) {
+		if (current_state->isFinal())
+			return getPath(paths, current.state, init_state_ptr);
+
+		for (SearchAction &action : current_state->actions()) {
 			// Stop if memory usage is too high
 			if (memUsageSucceeded(mem_limit_)) {
 				std::cout << "Memory limit exceeded (" << getCurrentRSS();
@@ -94,19 +103,22 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
 				return {};
 			}
 
-			SearchState new_state = action.execute(current.state);
+			std::shared_ptr<SearchState> new_state = std::make_shared<SearchState>(action.execute(*current_state));
 
-			// Don't add to frontier if already in closed
+			// Don't add to open if already in closed
 			if (closed.find(new_state) != closed.end())
 				continue;
 
-			auto stored_state = frontier.find(AStarFrontierItem(new_state));
-			double f_updated = current.g_cost + 1 + compute_heuristic(new_state, *heuristic_);
+			auto item = AStarFrontierItem(new_state);
+			auto stored_state = open.find(item);
+			double f_updated = new_g_cost + compute_heuristic(*new_state, *heuristic_);
 
-			if (stored_state == frontier.end() || stored_state->f_cost > f_updated) {
-				if (stored_state != frontier.end())
-					frontier.erase(stored_state); // Only if item was already in map
-				frontier.insert(AStarFrontierItem(new_state, current.g_cost + 1, f_updated));
+			if (stored_state == open.end() || stored_state->f_cost > f_updated) {
+				item.f_cost = f_updated;
+				item.g_cost = new_g_cost;
+				if (stored_state != open.end())
+					open.erase(stored_state); // Only if item was already in open
+				open.insert(item);
 				paths.insert_or_assign(new_state, PathItem(current.state, action));
 			}
 		}
